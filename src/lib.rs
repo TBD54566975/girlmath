9c2eba2ceb79e9a4d791177b30a309842f4ed467
Diane Huxley <dhuxley@tbd.email>
2023-11-03 22:13:45 -0700

    Use rusty_money instead of iso_currency

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,45 @@
-use iso_currency::Currency;
+use rusty_money::{Money, iso};
 use rust_decimal::Decimal;
 use std::str::FromStr;
 
 pub fn major_to_minor(currency_code: &str, amount: &str) -> Result<String, String> {
-    let currency = match Currency::from_str(currency_code) {
-        Ok(val) => val,
-        Err(_) => return Err("currency code not supported".to_string()),
+    let currency = match iso::find(currency_code) {
+        Some(val) => val,
+        None => return Err("currency code not supported".to_string()),
     };
 
-    let currency_symbol = currency.symbol().symbol;
-    let amount_formatted = amount
-        .replace(&currency_symbol, "")
-        .replace(",", "");
+    let multiplier = Decimal::from(10u32.pow(currency.exponent));
 
-    let decimal_amount = match Decimal::from_str(&amount_formatted) {
+    let amount_without_currency_symbol = amount.replace(&currency.symbol, "");
+    let amount_money = match Money::from_str(&amount_without_currency_symbol, currency) {
         Ok(val) => val,
         Err(_) => return Err("amount is not a decimal".to_string()),
     };
+    let amount_decimal = amount_money.amount();
 
-    let multiplier = match currency.exponent() {
-        Some(significant_digits) => Decimal::from(10u32.pow(significant_digits.into())),
-        None => Decimal::from(1), // some currencies don't have significant digits (e.g. JPY)
-    };
+    let minor_amount = amount_decimal * multiplier;
 
-    let minor_units = decimal_amount * multiplier;
-
-    return Ok(minor_units.floor().to_string());
+    return Ok(minor_amount.floor().to_string());
 }
 
 pub fn minor_to_major(currency_code: &str, amount: &str) -> Result<String, String> {
-    let currency = match Currency::from_str(currency_code) {
-        Ok(val) => val,
-        Err(_) => return Err("currency code not supported".to_string()),
+    let currency = match iso::find(currency_code) {
+        Some(val) => val,
+        None => return Err("currency code not supported".to_string()),
     };
 
-    let amount_without_subunit = match currency.symbol().subunit_symbol {
-        Some(subunit_symbol) => amount.replace(&subunit_symbol, ""),
-        None => amount.to_string(),
-    };
-    let amount_formatted = amount_without_subunit.replace(",", "");
-
-    let decimal_amount = match Decimal::from_str(&amount_formatted) {
+    let decimal_amount = match Decimal::from_str(amount) {
         Ok(val) => val,
         Err(_) => return Err("amount is not a decimal".to_string()),
     };
 
-    let multiplier = match currency.exponent() {
-        Some(significant_digits) => Decimal::from(10u32.pow(significant_digits.into())),
-        None => Decimal::from(1), // some currencies don't have significant digits (e.g. JPY)
-    };
+    let multiplier = Decimal::from(10u32.pow(currency.exponent));
 
     let major_units = decimal_amount / multiplier;
 
     return Ok(major_units.to_string());
 }
+
 
 #[cfg(test)]
 mod test {
@@ -119,7 +105,7 @@ mod test {
     #[test]
     fn euro_with_euro_symbol_and_thousands_comma() {
         let currency_code = "EUR";
-        let major_amount = "€12,345.67";
+        let major_amount = "€12.345,67";
         let minor_amount = major_to_minor(currency_code, major_amount).unwrap();
         assert_eq!(minor_amount, "1234567");
 
@@ -136,13 +122,5 @@ mod test {
 
         let result_major_amount = minor_to_major(currency_code, &minor_amount).unwrap();
         assert_eq!(result_major_amount, "12345");
-    }
-
-    #[test]
-    fn cents_with_symbol() {
-        let currency_code: &str = "USD";
-        let minor_amount = "99¢";
-        let major_amount = minor_to_major(currency_code, minor_amount).unwrap();
-        assert_eq!(major_amount, "0.99");
     }
 }
